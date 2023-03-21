new augmentations and fixed normalization

src/datamodules/weather_datamodule.py
@@ -41,26 +41,36 @@ class Dataset_RNN(Dataset):
         self.periods_forward = periods_forward
         self.history_length = history_length
         self.mode = mode
+        self.target = self.data
         # bins for pdsi
         self.boundaries = boundaries
+        if self.mode == "classification":
+            self.target = torch.bucketize(self.target, self.boundaries)
         # normalization
         if moments:
             self.moments = moments
             if normalize:
-                self.data = (self.data - self.moments[0][0])/(self.moments[0][1] - self.moments[0][0])
+                self.data = (self.data - self.moments[0][0]) / (
+                    self.moments[0][1] - self.moments[0][0]
+                )
                 i = 1
                 for feature in self.features:
-                    feature = (feature  - self.moments[i][0])/(self.moments[i][1] - self.moments[i][0])
+                    feature = (feature - self.moments[i][0]) / (
+                        self.moments[i][1] - self.moments[i][0]
+                    )
                     i += 1
         else:
             self.moments = []
             if normalize:
-                self.data = (self.data - torch.min(self.data))/(torch.max(self.data) - torch.min(self.data))
+                self.data = (self.data - torch.min(self.data)) / (
+                    torch.max(self.data) - torch.min(self.data)
+                )
                 self.moments.append((torch.min(self.data), torch.max(self.data)))
                 for feature in self.features:
-                    feature = (feature - torch.min(feature))/(torch.max(feature) - torch.min(feature))
+                    feature = (feature - torch.min(feature)) / (
+                        torch.max(feature) - torch.min(feature)
+                    )
                     self.moments.append((torch.min(feature), torch.max(feature)))
-
 
     def __len__(self):
         return len(self.data) - self.periods_forward - self.history_length
@@ -71,12 +81,10 @@ class Dataset_RNN(Dataset):
             input_tensor = torch.cat(
                 (input_tensor, feature[idx : idx + self.history_length]), dim=0
             )
-            
-        target = self.data[
+
+        target = self.target[
             idx + self.history_length : idx + self.history_length + self.periods_forward
         ]
-        if self.mode == "classification":
-            target = torch.bucketize(target, self.boundaries)
 
         return (
             input_tensor,
@@ -146,7 +154,7 @@ class WeatherDataModule(LightningDataModule):
         self.transforms = transforms.Compose(
             [
                 transforms.Resize((self.n_cells_hor, self.n_cells_ver)),
-                #        transforms.Normalize((0.0), (1.0)),
+                # transforms.RandomErasing(p=0.25,scale=(0.1, 0.3), ratio=(0.5,2),value=0),
             ]
         )
         self.time_col = time_col
@@ -165,7 +173,7 @@ class WeatherDataModule(LightningDataModule):
         self.feature_to_predict = feature_to_predict
         self.num_of_features = num_of_additional_features + 1
         self.additional_features = additional_features
-        self.boundaries = boundaries
+        self.boundaries = torch.Tensor(boundaries)
         self.normalize = normalize
 
         self.batch_size = batch_size
@@ -253,6 +261,7 @@ class WeatherDataModule(LightningDataModule):
                 self.transforms,
                 self.mode,
                 self.normalize,
+                [],
             )
             # valid_end = int(
             #     (self.train_val_test_split[0] + self.train_val_test_split[1])

src/models/rcnn_module.py
@@ -54,6 +54,7 @@ class RCNNModule(LightningModule):
         boundaries: List[int] = [-2],
         num_classes: int = 2,
         values_range: int = 10,
+        dropout: float = 0.0,
         lr: float = 0.003,
         weight_decay: float = 0.0,
     ):
@@ -175,7 +176,7 @@ class RCNNModule(LightningModule):
                 dilation=1,
                 groups=1,
             ),
-            #nn.Softmax(dim=1),
+            # nn.Softmax(dim=1),
         )
 
         self.register_buffer(
@@ -212,6 +213,7 @@ class RCNNModule(LightningModule):
     def forward(self, x: torch.Tensor):
         prev_c = self.prev_state_c
         prev_h = self.prev_state_h
+        x = torch.nn.Dropout2d(p=self.dropout)
         x_emb = self.embedding(x)
         x_and_h = torch.cat([prev_h, x_emb], dim=1)
 
@@ -239,7 +241,7 @@ class RCNNModule(LightningModule):
         x, y = batch
         preds = self.forward(x)
         # atm checking last prediction
-        loss = self.criterion(preds, y[:,-1,:,:])
+        loss = self.criterion(preds, y[:, -1, :, :])
         return loss, preds, y[:, -1, :, :]
 
     def rolling_step(self, batch: Any):
@@ -253,15 +255,17 @@ class RCNNModule(LightningModule):
         for i in range(1, self.periods_forward):
             x = torch.cat((x[:, 1:, :, :], rolling[:, None, :, :]), dim=1)
             rolling = torch.mean(x, dim=1)
-            rolling_forecast = torch.cat((rolling_forecast, rolling[:, None, :, :]), dim=1)
+            rolling_forecast = torch.cat(
+                (rolling_forecast, rolling[:, None, :, :]), dim=1
+            )
 
         return rolling_forecast
-    
+
     def class_baseline(self, batch: Any):
         x, y = batch
         # return most frequent class along history_dim
-        x_binned = torch.bucketize(x, self.boundaries) 
-        most_freq_values, most_freq_indices =  torch.mode(x_binned, dim=1)
+        x_binned = torch.bucketize(x, self.boundaries)
+        most_freq_values, most_freq_indices = torch.mode(x_binned, dim=1)
         return most_freq_values
 
     def on_after_backward(self) -> None:
@@ -286,7 +290,7 @@ class RCNNModule(LightningModule):
             all_preds = torch.cat((all_preds, outputs[i]["preds"]), 0)
             all_targets = torch.cat((all_targets, outputs[i]["targets"]), 0)
         all_preds = torch.softmax(all_preds, dim=1)
-        all_preds = all_preds[:,1,:,:]
+        all_preds = all_preds[:, 1, :, :]
         rocauc_table = rocauc_celled(all_targets, all_preds)
         # log metrics
         if self.mode == "classification":
@@ -314,14 +318,13 @@ class RCNNModule(LightningModule):
                 on_epoch=True,
                 prog_bar=True,
             )
-        
+
         # log metrics
         # r2table = rsquared(all_targets, all_preds, mode="mean")
         # self.log("train/R2_std", np.std(r2table), on_epoch=True, prog_bar=True)
         # self.log("train/R2", np.median(r2table), on_epoch=True, prog_bar=True)
         # self.log("train/R2_min", np.min(r2table), on_epoch=True, prog_bar=True)
         # self.log("train/R2_max", np.max(r2table), on_epoch=True, prog_bar=True)
-
 
     def validation_step(self, batch: Any, batch_idx: int):
         loss, preds, targets = self.step(batch)
@@ -330,16 +333,16 @@ class RCNNModule(LightningModule):
         return {"loss": loss, "preds": preds, "targets": targets}
 
     def validation_epoch_end(self, outputs: List[Any]):
-        
+
         all_targets = outputs[0]["targets"]
         all_preds = outputs[0]["preds"]
 
         for i in range(1, len(outputs)):
             all_preds = torch.cat((all_preds, outputs[i]["preds"]), 0)
             all_targets = torch.cat((all_targets, outputs[i]["targets"]), 0)
-        
+
         all_preds = torch.softmax(all_preds, dim=1)
-        all_preds = all_preds[:,1,:,:]
+        all_preds = all_preds[:, 1, :, :]
         rocauc_table = rocauc_celled(all_targets, all_preds)
         # log metrics
         if self.mode == "classification":
@@ -367,7 +370,6 @@ class RCNNModule(LightningModule):
                 on_epoch=True,
                 prog_bar=True,
             )
-        
 
         # log metrics
         # r2table = rsquared(all_targets, all_preds, mode="mean")
@@ -387,7 +389,7 @@ class RCNNModule(LightningModule):
         return {"loss": loss, "preds": preds, "targets": targets, "baseline": baseline}
 
     def test_epoch_end(self, outputs: List[Any]):
-        
+
         all_targets = outputs[0]["targets"]
         all_baselines = outputs[0]["baseline"]
         all_preds = outputs[0]["preds"]
@@ -396,9 +398,9 @@ class RCNNModule(LightningModule):
             all_preds = torch.cat((all_preds, outputs[i]["preds"]), 0)
             all_targets = torch.cat((all_targets, outputs[i]["targets"]), 0)
             all_baselines = torch.cat((all_baselines, outputs[i]["baseline"]), 0)
-        
+
         all_preds = torch.softmax(all_preds, dim=1)
-        all_preds = all_preds[:,1,:,:]
+        all_preds = all_preds[:, 1, :, :]
         rocauc_table = rocauc_celled(all_targets, all_preds)
         rocauc_table_baseline = rocauc_celled(all_targets, all_baselines)
         # log metrics
@@ -445,7 +447,7 @@ class RCNNModule(LightningModule):
                 on_epoch=True,
                 prog_bar=True,
             )
-        
+
         # log metrics
         # test_r2table = rsquared(all_targets, all_preds, mode="full")
         # self.log("test/R2_std", np.std(test_r2table), on_epoch=True, prog_bar=True)

src/utils/metrics.py
@@ -1,7 +1,9 @@
 import numpy as np
 import torch
 from sklearn.metrics import r2_score, roc_auc_score
-#from torchmetrics.classification import BinaryAUROC
+
+# from torchmetrics.classification import BinaryAUROC
+
 
 def rmse(target, pred):
 
@@ -16,7 +18,7 @@ def smape(target, pred):
 
 def rsquared(target, pred, mode: str = "mean"):
 
-    if (mode == "full"):
+    if mode == "full":
         # log table
         r2table = np.zeros((pred.shape[2], pred.shape[3]))
         for x in range(pred.shape[2]):
@@ -28,33 +30,30 @@ def rsquared(target, pred, mode: str = "mean"):
                 r2table[x][y] /= pred.shape[0]
         return r2table
 
-    elif (mode == "mean"):
+    elif mode == "mean":
         pred_mean = torch.mean(pred, (2, 3)).cpu().numpy()
         target_mean = torch.mean(target, (2, 3)).cpu().numpy()
 
         r2_raw = np.zeros(pred_mean.shape[0])
-        
+
         for b in range(pred_mean.shape[0]):
             r2_raw[b] = r2_score(target_mean[b, :], pred_mean[b, :])
-        #r2_median = np.median(r2_raw)
+        # r2_median = np.median(r2_raw)
 
         return r2_raw
 
 
 def rocauc_celled(all_targets, all_preds):
 
-    # probs for class 1
-    all_preds = torch.softmax(all_preds, dim=1)
-    all_preds = all_preds[:,1,:,:]
-    rocauc_table = torch.zeros(all_preds.shape[1],all_preds.shape[2])
+    rocauc_table = torch.zeros(all_preds.shape[1], all_preds.shape[2])
     for x in range(all_preds.shape[1]):
         for y in range(all_preds.shape[2]):
             try:
-                rocauc_table[x][y] = roc_auc_score(all_targets[:,x,y].cpu().numpy(), all_preds[:,x,y].cpu().numpy())
+                rocauc_table[x][y] = roc_auc_score(
+                    all_targets[:, x, y].cpu().numpy(), all_preds[:, x, y].cpu().numpy()
+                )
             except:
-                rocauc_table[x][y] = float("nan") #0
-            #rocauc_table[x][y] = BinaryAUROC(all_preds[:,x,y], all_targets[:,x,y])
+                rocauc_table[x][y] = 0  # float("nan") #0
+            # rocauc_table[x][y] = BinaryAUROC(all_preds[:,x,y], all_targets[:,x,y])
 
     return rocauc_table
-
-